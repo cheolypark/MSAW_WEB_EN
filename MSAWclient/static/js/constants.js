83187c1675ddde45d7a370119e817bf6df362075
Jumi <Jumi@WINDOWS-HS6OIEU.fios-router.home>
2017-10-21 16:48:26 -0400

    Settings were changed.

--- MSAWclient/static/js/constants.js
+++ MSAWclient/static/js/constants.js
@@ -1,3 +1,3 @@
-var BACKEND_URL="http://localhost:9019/api/";
-var DOWNLOAD_URL_prediction="http://localhost:9019/api/downloadprediction/";
-var DOWNLOAD_URL_optimal="http://localhost:9019/api/downloadoptimal/";
+var BACKEND_URL="http://localhost:8080/api/";
+var DOWNLOAD_URL_prediction="http://localhost:8080/api/downloadprediction/";
+var DOWNLOAD_URL_optimal="http://localhost:8080/api/downloadoptimal/";
